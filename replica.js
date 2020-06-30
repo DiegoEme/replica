cccf277c0bf30c5d732ecd817cb08478810d9ad7
Diego <soterg.uhdeia@gmail.com>
2020-06-29 23:08:26 -0500

    Ultima version

--- replica.js
+++ replica.js
@@ -2,7 +2,3 @@ document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".carousel");
   var instances = M.Carousel.init(elems, {});
 });
-
-var instance = M.Carousel.init({
-  fullWidth: true,
-});
